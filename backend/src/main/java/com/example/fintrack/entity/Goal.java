8444aa20e32fcb6540abb81445248510e20af04b
Robocoders <team@blackbox.ai>
2024-10-19 11:17:50 +0000

    Add Lombok @Getter and @Setter annotations to entity classes

--- backend/src/main/java/com/example/fintrack/entity/Goal.java
+++ backend/src/main/java/com/example/fintrack/entity/Goal.java
@@ -2,10 +2,14 @@
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Goal {
     // existing fields and methods
